d62ee06ac015243f5ddcc295aada83b33a08330d
Josip Domazet <josip.domazet.corp@gmail.com>
2023-10-10 23:06:23 +0200

    about tab

--- screens/AboutScreen.tsx
+++ screens/AboutScreen.tsx
@@ -1,12 +1,55 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Image, StyleSheet, Text, View} from 'react-native';
+import packageInfo from '../package.json';
 
 const AboutScreen = () => {
+    const appVersion = packageInfo.version;
+
+    const developerInfo = {
+        name: 'Josip Domazet',
+        bio: 'Full Stack Developer',
+        email: 'josip.domazet.corp@gmail.com',
+    };
+
     return (
-        <View>
-            <Text>About Screen</Text>
+        <View style={styles.container}>
+            <Image source={require('../assets/profile.jpg')} style={styles.profileImage}/>
+            <Text style={styles.name}>{developerInfo.name}</Text>
+            <Text style={styles.bio}>{developerInfo.bio}</Text>
+            <Text style={styles.email}>{developerInfo.email}</Text>
+            <Text style={styles.version}>App Version: {appVersion}</Text>
         </View>
     );
 };
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    profileImage: {
+        width: 150,
+        height: 150,
+        borderRadius: 75,
+        marginBottom: 20,
+    },
+    name: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginBottom: 10,
+    },
+    bio: {
+        fontSize: 18,
+        marginBottom: 10,
+    },
+    email: {
+        fontSize: 16,
+        marginBottom: 10,
+    },
+    version: {
+        fontSize: 14,
+    },
+});
 
 export default AboutScreen;
